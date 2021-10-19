82c7e4c23e40adc173c4e71f5ddbf4e4926a3486
KerimM-bit <kmirzeyev30@gmail.com>
2021-10-19 17:11:15 +0400

    Ilk Ders Son

---
+++ src/app.js
@@ -0,0 +1,34 @@
+console.log("Merhaba Kodlama.io")
+// JS type safe degildir...
+console.log('Kamona')
+
+console.log('Pakaa')
+
+console.log('KEEEEEAAANNNNNNYYYYYYY')
+
+console.log('Oi, Oi, Oi')
+
+
+//dolarDun = 'Istanbul'
+let dolarBugun = 9.33
+
+
+let dolarDun = 9.20
+
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11
+let animes = ['Attack On Titan', 'Black Clover', 'Fairy Tail', 'Demonslayer', 'Death Note']
+
+console.log(euroDun)
+console.log("<ul>")
+for (let i = 0; i<animes.length;i++){
+    console.log("<li"+animes[i]+"</li>")
+}
+console.log("</ul>")
